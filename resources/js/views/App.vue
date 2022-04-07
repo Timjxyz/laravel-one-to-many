9839606ff462d3c1db91e8ad186f8358ca857fc9
Timothy Gaither <timjgaither@gmail.com>
2022-04-07 13:15:24 +0200

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,16 @@
+<template>
+<!-- vista in front di vue -->
+  <div>
+      <h1>Work in progress</h1>
+      <h2>Pagina in elaborazione</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style>
+</style>
